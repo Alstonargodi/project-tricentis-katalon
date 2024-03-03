46510712f5c33f599453484e3737f316d52957e2
Alstonargodi <55945729+Alstonargodi@users.noreply.github.com>
2024-03-03 14:32:09 +0700

    add: transaction test suite / payment with credit card

--- Scripts/transaction/normal/transaction_credit/Script1709354964747.groovy
+++ Scripts/transaction/normal/transaction_credit/Script1709354964747.groovy
@@ -17,7 +17,67 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.callTestCase(findTestCase('cart/normal/cart_insert'), [:], FailureHandling.STOP_ON_FAILURE)
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://demowebshop.tricentis.com/')
+
+WebUI.click(findTestObject('Object Repository/loginpage/menu_login'))
+
+WebUI.comment('Login page')
+
+WebUI.verifyElementPresent(findTestObject('loginpage/div_login_page'), 0)
+
+WebUI.verifyTextPresent('Welcome, Please Sign In!', false)
+
+WebUI.verifyElementVisible(findTestObject('loginpage/textfield_email'))
+
+WebUI.setText(findTestObject('Object Repository/loginpage/textfield_email'), username)
+
+WebUI.verifyElementVisible(findTestObject('loginpage/textfield_password'))
+
+WebUI.setText(findTestObject('loginpage/textfield_password'), password)
+
+WebUI.verifyElementVisible(findTestObject('loginpage/checkbox_rememberme'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.click(findTestObject('Object Repository/loginpage/checkbox_rememberme'))
+
+WebUI.verifyElementChecked(findTestObject('cartpage/checkbox_rememberme'), 1)
+
+WebUI.verifyElementClickable(findTestObject('loginpage/button_login'))
+
+WebUI.click(findTestObject('Object Repository/loginpage/button_login'))
+
+WebUI.comment('Insert a item to cart')
+
+WebUI.verifyElementPresent(findTestObject('cartpage/div_featured_product'), 0)
+
+WebUI.click(findTestObject('cartpage/div_product_item'))
+
+WebUI.verifyElementPresent(findTestObject('cartpage/textfield_recipient_name'), 0)
+
+WebUI.verifyElementPresent(findTestObject('cartpage/textfield_recipient_email'), 0)
+
+WebUI.verifyElementPresent(findTestObject('cartpage/textfield_sender_name'), 0)
+
+WebUI.verifyElementPresent(findTestObject('cartpage/textfield_sender_email'), 0)
+
+WebUI.verifyElementPresent(findTestObject('cartpage/button_quatity_cart'), 0)
+
+WebUI.setText(findTestObject('Object Repository/cartpage/textfield_recipient_name'), recipient_name)
+
+WebUI.setText(findTestObject('Object Repository/cartpage/textfield_recipient_email'), recipient_email)
+
+WebUI.setText(findTestObject('cartpage/textfield_sender_name'), sender_name)
+
+WebUI.setText(findTestObject('cartpage/textfield_sender_email'), sender_email)
+
+WebUI.setText(findTestObject('cartpage/textfield_message'), sender_message)
+
+WebUI.click(findTestObject('Object Repository/cartpage/button_quatity_cart'))
+
+WebUI.verifyElementVisible(findTestObject('cartpage/label_add_success'))
+
+WebUI.comment('transacition page')
 
 WebUI.verifyElementPresent(findTestObject('transactionpage/menu_shopping_cart'), 0)
 
@@ -41,17 +101,17 @@ WebUI.verifyElementPresent(findTestObject('transactionpage/div_payment_info'), 0
 
 WebUI.verifyElementPresent(findTestObject('transactionpage/div_payment_method'), 0)
 
-WebUI.selectOptionByValue(findTestObject('Object Repository/transactionpage/option_credit_card'), 'Visa', true)
+WebUI.selectOptionByValue(findTestObject('Object Repository/transactionpage/option_credit_card'), card_type, true)
 
 WebUI.setText(findTestObject('transactionpage/textfield_card_holder_name'), 'wayan')
 
-WebUI.setText(findTestObject('transactionpage/textfield_card_number'), '4001919257537193')
+WebUI.setText(findTestObject('transactionpage/textfield_card_number'), card_number)
 
-WebUI.selectOptionByValue(findTestObject('Object Repository/transactionpage/option_month'), '2026', true)
+WebUI.selectOptionByValue(findTestObject('Object Repository/transactionpage/option_month'), card_year, true)
 
-WebUI.selectOptionByValue(findTestObject('Object Repository/transactionpage/option_year'), '9', true)
+WebUI.selectOptionByValue(findTestObject('Object Repository/transactionpage/option_year'), card_month, true)
 
-WebUI.setText(findTestObject('transactionpage/textfield_card_code'), '3525')
+WebUI.setText(findTestObject('transactionpage/textfield_card_code'), card_code)
 
 WebUI.click(findTestObject('transactionpage/button_next_confirm'))
 
